f4b9ebce614d49333eb704274610bb36fd373e19
n3cac <chocolatey030@gmail.com>
2020-07-25 01:02:09 +0200

    [GH-5] Delete anonymous user.

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 nexus(){
-  source <(curl -L https://gist.githubusercontent.com/030/666c99d8fc86e9f1cc0ad216e0190574/raw/c5faae82960c46c232a099231166e1b2fc3bb0bb/nexus-docker.sh)
+  curl -L https://gist.githubusercontent.com/030/666c99d8fc86e9f1cc0ad216e0190574/raw/c5faae82960c46c232a099231166e1b2fc3bb0bb/nexus-docker.sh -o start.sh
+  chmod +x start.sh
+  source ./start.sh 3.25.0
 }
 
 delete(){
@@ -18,10 +20,24 @@ create(){
 --data '{"name":"REPO_NAME","online":true,"storage":{"blobStoreName":"default","strictContentTypeValidation":true,"writePolicy":"ALLOW_ONCE"},"maven": {"versionPolicy": "RELEASE","layoutPolicy": "STRICT"}}'
 }
 
+anonymous(){
+  echo "Disable anonymous access..."
+  curl -u admin:$PASSWORD -X PUT "http://localhost:9999/service/rest/beta/security/anonymous" \
+-H  "accept: application/json" \
+-H  "Content-Type: application/json" \
+--data '{"enabled":false}'
+}
+
+debug(){
+  echo $PASSWORD
+  sleep 60
+}
+
 main(){
   nexus
   delete
   create
+  anonymous
 }
 
 main
